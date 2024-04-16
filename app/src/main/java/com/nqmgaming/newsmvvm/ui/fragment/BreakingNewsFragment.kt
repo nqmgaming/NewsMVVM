a533756d678d7be6863b377f5e68413ba12a93d6
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-04-16 17:15:21 +0700

    set up navigation

---
+++ app/src/main/java/com/nqmgaming/newsmvvm/ui/fragment/BreakingNewsFragment.kt
@@ -0,0 +1,22 @@
+package com.nqmgaming.newsmvvm.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nqmgaming.newsmvvm.R
+
+
+class BreakingNewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_breaking_news, container, false)
+    }
+
+
+}
