15a918774a3a885c2e6ad3f7c8e68163c3f026c0
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-04-16 17:41:07 +0700

    set up api

---
+++ app/src/main/java/com/nqmgaming/newsmvvm/api/NewsApi.kt
@@ -0,0 +1,31 @@
+package com.nqmgaming.newsmvvm.api
+
+import com.nqmgaming.newsmvvm.model.NewsResponse
+import com.nqmgaming.newsmvvm.util.Constant.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "vn",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKe: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    )
+
+}
---
+++ app/src/main/java/com/nqmgaming/newsmvvm/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.nqmgaming.newsmvvm.util
+
+class Constant {
+    companion object {
+        const val API_KEY = "d90c4ce36eb84a2283b79ebba22a55c2"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
